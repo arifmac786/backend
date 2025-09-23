2d973897da0db58e1e6ba663d287ed0137f72b8f
arifmac786 <arifmac78692@gmail.com>
2025-09-23 09:17:44 +0530

    configuration setup for managing data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 const app = express()
 
+// for connecting frontend and backend
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  credentials:true
+}))
 
+// for excepting json data from frontend
+app.use(express.json({
+  limit: "16kb"
+}))
+
+// for excepting data from url
+app.use(express.urlencoded({          // extended --> object inside object
+  extended:true,
+  limit:"16kb"})) 
+
+//for storing file like pdf images and so on (public assets)
+app.use(express.static("public"))  
+
+// for accessing user browser cookie and work on that.(from server only server can read and delete)
+app.use(cookieParser())
 
 
 
