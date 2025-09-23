8b758108d806732c67d3a52d9facb442a272ec4d
arifmac786 <arifmac78692@gmail.com>
2025-09-23 20:38:49 +0530

    token (jsonwebtoken)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,4 +59,33 @@ userSchema.methods.isPasswordCorrect = async function (password){    //function
   return await bcrypt.compare(password,this.password) //true or false
 }
 
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      username:this.username,
+      fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+
+}
+userSchema.methods.generateRefreshToken = function(){
+     return jwt.sign(
+    {
+      _id:this._id,
+      
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_SECRET
+    }
+  )
+
+}
+
 export const User = mongoose.model("User",userSchema)
