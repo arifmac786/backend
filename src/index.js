35c561c77912434c223fa7ce7f4d9dfec814d6fe
arifmac786 <arifmac78692@gmail.com>
2025-09-23 09:00:11 +0530

    app listening code setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import { app } from './app.js'
 dotenv.config({
   path:'./env'
 })
+let port = process.env.PORT || 8000
 
+ 
 connectDB()
+.then(()=>{
+  app.listen(port, ()=>{
+    console.log(`Server is running at PORT : ${port}`);
+    
+  })
+})
+.catch((error)=>{
+  console.log("MONGODB connection failed !!! " , error);
+  
+})
